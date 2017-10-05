3a829492475a6d33bd7834af82a767e46f19d2bb
Scott Foster <thescottfoster@gmail.com>
2017-10-05 01:22:09 -0500

    added most of it

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,24 +2,26 @@
 
 var gameState = 'joinGame';
 
-var currentName = '';
+var currentName = 'Spectator';
 
 var currentSelection = '';
 
+var playerNumber = null;
 
 var config = {
-    apiKey: "AIzaSyAFv-QDxJ7R061XotHbP0yWhvKdo5dkukU",
-    authDomain: "rps-multiplayer-db.firebaseapp.com",
-    databaseURL: "https://rps-multiplayer-db.firebaseio.com",
-    projectId: "rps-multiplayer-db",
-    storageBucket: "rps-multiplayer-db.appspot.com",
-    messagingSenderId: "1005144939725"
-  };
+	apiKey: "AIzaSyAFv-QDxJ7R061XotHbP0yWhvKdo5dkukU",
+	authDomain: "rps-multiplayer-db.firebaseapp.com",
+	databaseURL: "https://rps-multiplayer-db.firebaseio.com",
+	projectId: "rps-multiplayer-db",
+	storageBucket: "rps-multiplayer-db.appspot.com",
+	messagingSenderId: "1005144939725"
+};
 
 firebase.initializeApp(config);
 
 database = firebase.database();
 
+playerObject = '';
 
 
 /* --- --- --- GLOBAL FUNCTION --- --- --- */
@@ -29,41 +31,138 @@ function drawScreen(){
 
 	switch(gameState){
 
+
 	//draw joinGame screen
-		case 'joinGame':
+	case 'joinGame':
 
-	//clear main panel
+	break;
 
-	//create, append #join-button
-		
-		break;
 
 	//draw rpsSelect screen
-		case 'rpsSelect':
+	case 'rpsSelect':
 
-		//clear main panel
+	$('#join-panel').hide();
 
 		//create, append RPS buttons
 
 		break;
 
+
 	//draw results screen
-		case 'results':
+	case 'results':
 
-		//clear main panel
 
-		//display selections
 
-		//display Win / Lose / Tie
+		// display Win / Lose / Tie
 
-		// set five second timer, then
+		// set three second timer, then
+
+		// clear panel
 
 		// set gameState to rpsSelect, drawScreen
+
+		database.ref('game-round').set(1);
 
 	}
 
 
 }
+
+//checks if both players are logged in and updates round in db
+
+function checkIfReady(){
+
+	database.ref().once("value", function(snapshot){
+
+		if( snapshot.child('playerDbOne').val() !== null && snapshot.child('playerDbTwo').val() !== null ){
+			console.log('EveryBodyready')
+		}
+
+		database.ref('game-round').set(1);
+
+	});
+
+}
+
+
+
+//check if both players have a guess, if so perform the math
+function checkIfGuessed(){
+
+	database.ref().once("value", function(snapshot){
+
+		if( snapshot.child('playerDbOne/guess').val() !== null && snapshot.child('playerDbTwo/guess').val() !== null ){
+			console.log('EveryBody Guessed')
+			database.ref('game-round').set(2);
+		}
+
+		
+
+	});
+
+}
+
+
+//compare the guesses in dB, update W/L
+
+function compareGuesses(){
+
+	database.ref().once("value", function(snapshot){
+		var playerOneGuess = snapshot.child('playerDbOne/guess').val();
+		var playerTwoGuess = snapshot.child('playerDbTwo/guess').val();
+		var bothGuesses = playerOneGuess + playerTwoGuess;
+
+		console.log(bothGuesses);
+
+		if( playerOneGuess === playerTwoGuess){
+			database.ref('result').set('tie');
+
+			if(playerNumber === 1){
+				database.ref('playerDbOne/guess').set(null);		
+			}
+
+			if(playerNumber === 2){
+				database.ref('playerDbTwo/guess').set(null);		
+			}
+
+		}
+
+		else if ( bothGuesses === 'rockscissors' || bothGuesses === 'paperrock' || bothGuesses === 'scissorspaper'){
+			console.log('Player One Wins')
+			database.ref('result').set('player1wins');
+
+			if(playerNumber === 1){
+				database.ref('playerDbOne/guess').set(null);
+				database.ref('playerDbOne/wins').set(snapshot.child('playerDbOne/wins').val()+1)				
+			}
+
+			if(playerNumber === 2){
+				database.ref('playerDbTwo/guess').set(null);
+				database.ref('playerDbTwo/losses').set(snapshot.child('playerDbTwo/losses').val()+1)				
+			}
+		}
+		
+		else{
+			console.log('Player Two Wins');
+			database.ref('result').set('player2wins');
+
+			if(playerNumber === 1){
+				database.ref('playerDbOne/guess').set(null);
+				database.ref('playerDbOne/losses').set(snapshot.child('playerDbOne/losses').val()+1)		
+			}
+
+			if(playerNumber === 2){
+				database.ref('playerDbTwo/guess').set(null);	
+				database.ref('playerDbTwo/wins').set(snapshot.child('playerDbTwo/wins').val()+1)			
+			}
+		}
+
+	});
+
+	database.ref('game-round').set(3);
+	
+}
+
 
 /* --- --- --- MAIN LOGIC --- --- --- */
 
@@ -76,40 +175,105 @@ function drawScreen(){
 /* --- --- --- EVENT HANDLERS --- --- --- */
 
 
-//when Join button clicked
 
-$(document).on('click', '#join-button', function(event){
 
-	console.log('join');
-//check db; if playerOne empty, then playerName = playerOne
+//when Name Submit button clicked
 
-//else if playerTwo empty, become playerTwo
+$(document).on('click', '#name-submit', function(event){
 
-//else say Game is Full
+	event.preventDefault();
 
-//set gameState to RPS select, drawScreen()
+	//initialize input name to value
+	var nameInput = $('#name-input').val();
+
+	currentName = nameInput;
+
+	//clears form input
+	$('#name-input').val('');
+
+	console.log(nameInput);
+
+
+	//get snapshot of db
+	database.ref().once("value", function(snapshot) {
+
+		
+		//if player one not existing, create player one
+		if( snapshot.child('playerDbOne').val() === null ){
+			
+			database.ref('playerDbOne').set({
+				name: nameInput,
+				wins: 0,
+				losses: 0,
+				guess: null,
+				player: 1
+			});
+
+			playerObject = database.ref('playerDbOne');
+			playerObject.onDisconnect().remove();
+
+			$('#player-one-name').addClass('my-name');
+
+			playerNumber = 1;
+			
+			checkIfReady();
+			drawScreen();
+		}
+
+		//otherwise create player two
+		else if( snapshot.child('playerDbTwo').val() === null ){
+			
+			database.ref('playerDbTwo').set({
+				name: nameInput,
+				wins: 0,
+				losses: 0,
+				player: 2,
+				guess: null
+			});
+
+			playerObject = database.ref('playerDbTwo');
+			playerObject.onDisconnect().remove();
+
+			$('#player-two-name').addClass('my-name');
+
+			playerNumber = 2;
+
+			checkIfReady();
+			drawScreen();
+			
+		}
+
+		//otherwise alert that game is full
+		else{
+			alert('Game is currently full; try again later.');
+		}
+
+	});
 
 });
 
 
-$(document).on('click', '#new-game-button', function(event){
-
-	console.log('restart')
-
-// set all counters to 0
-
-// reset counters, players in db
-
-// set gameState = joinGame, drawScreen()
-
-});
 
 
 $(document).on('click', '.rps-selector', function(event){
 
-	console.log('selected ' +$(this).attr('data-name'));
+	console.log(currentName +' selected ' +$(this).attr('data-name'));
 
 	// set player choice in db to correct choice
+
+	if ( playerNumber === 1){
+
+		database.ref('playerDbOne').update({guess: $(this).attr('data-name') })
+
+	}
+
+	else if ( playerNumber === 2){
+
+		database.ref('playerDbTwo').update({guess: $(this).attr('data-name') })
+
+	}
+
+	checkIfGuessed();
 
 	// if both player choices are set,
 
@@ -121,47 +285,124 @@ $(document).on('click', '.rps-selector', function(event){
 
 		// increment rounds in db
 
+	});
+
+
+//chat submit button
+$(document).on('click', '#chat-submit', function(event){
+
+	event.preventDefault();
+
+	//initialize input name to value
+	var chatInput = currentName +': ' +$('#chat-input').val();
+	console.log(chatInput);
+
+	$('#chat-input').val('');
+
+	database.ref('chat-input/current').set(chatInput);
+
 });
 
 
-//event listener for rounds in db
 
-	//on change
-		
-		//gameState = results, drawscreen
+
+
+// draws playerNames to screen
+
+database.ref().on("child_added", function(snapshot) {
+
+	if (snapshot.val().player === 1){
+		$('#player-one-name').text(snapshot.val().name)
+	}
+
+
+	if (snapshot.val().player === 2){
+		$('#player-two-name').text(snapshot.val().name)
+	}
+
+});
+
+
+//erases playernames on disconnect
+database.ref().on("child_removed", function(snapshot) {
+
+	console.log( snapshot.val().player);
+
+	if (snapshot.val().player === 1){
+		$('#player-one-name').text('Player One')
+	}
+
+
+	if (snapshot.val().player === 2){
+		$('#player-two-name').text('Player Two')
+	}
+
+	database.ref('game-round').set(0);
+
+});
+
+
+//watches game round
+database.ref('game-round').on("value", function(snapshot){
+
+	console.log(snapshot.val())
+
+	if(snapshot.val() === 1 && playerNumber !== null){
+		gameState = 'rpsSelect';
+		drawScreen();
+	}
+
+	else if(snapshot.val() === 2 ){
+		compareGuesses();
+	}
+
+	else if (snapshot.val() === 3 ){
+
+		gameState = 'results';
+		drawScreen();
+	}
+
+})
+
+
+
+//watchers for W/L
+
+database.ref('playerDbOne/wins').on("value", function(snapshot){
+	if(snapshot.val() !== null){
+		$('#player-one-wins').text('Wins: '+snapshot.val());
+	}
+});
+
+database.ref('playerDbOne/losses').on("value", function(snapshot){
+	if(snapshot.val() !== null){
+		$('#player-one-losses').text('Losses: '+snapshot.val());
+	}
+});
+
+database.ref('playerDbTwo/wins').on("value", function(snapshot){
+	if(snapshot.val() !== null){
+		$('#player-two-wins').text('Wins: '+snapshot.val());
+	}
+});
+
+database.ref('playerDbTwo/losses').on("value", function(snapshot){
+	if(snapshot.val() !== null){
+		$('#player-two-losses').text('Losses: '+snapshot.val());
+	}
+});
+
+
+//watcher for chat
+
+database.ref('chat-input').on("child_changed", function(snapshot){
+	// if(snapshot.val() !== null){
+		$('#chat-box').prepend('<p>' +snapshot.val() +'</p>');
+	// }
+});
+
 
 
 /* --- --- --- PSEUDO / NOTES
-
-On Page Load:
-
-* prompt for name
-* check database for other player
-* give RPS options
-	wait for second player
-* show results
-
-
-
-
-
-On "Restart Game":
-	reset counters in db
-	set 
-
-
-
-Game Modes:
-
-Join Game
-
-RPS Choice
-
-Results
-
-
-
-
-
 
 */
